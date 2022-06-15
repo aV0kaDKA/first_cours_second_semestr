dfa5d0316656f3af3e4ddecfd60350db34f66225
Гуртовая Анна <103031042+aV0kaDKA@users.noreply.github.com>
2022-06-15 09:35:09 +0300

    Add files via upload

---
+++ DZ11/DZ11.cpp
@@ -0,0 +1,21 @@
+﻿#include "Header.h"
+int main()
+{
+    setlocale(0, "");
+    srand(time(NULL));
+    personal_computeer q, w;
+    notebook a, s;
+    phone z, x;
+    fill_rand(q);
+    fill_rand(w);
+    fill_rand(a);
+    fill_rand(s);
+    fill_rand(z);
+    fill_rand(x);
+    q.print();
+    w.print();
+    a.print();
+    s.print();
+    z.print();
+    x.print();
+}
