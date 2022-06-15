ef93f075e7e324af6f18b6ebde3a27f07e6832ed
Гуртовая Анна <103031042+aV0kaDKA@users.noreply.github.com>
2022-06-15 09:24:07 +0300

    Add files via upload

---
+++ DZ10/DZ10.cpp
@@ -0,0 +1,10 @@
+﻿#include "Header.h"
+int main() {
+	setlocale(0, "");
+	Human human("Борисова", "Елена", "Ивановна", 50);
+	Student student("Гуртовая", "Анна", "Алекссевна", 19, 1);
+	Boss boss("Алексей", "Гуртовой", "Константинович", 50, 200);
+	human.print();
+	student.print();
+	boss.print();
+}
